4176ba086b37e6f53c40b9757e056d42b44e8443
Gigabyte1027 <68049346+Gigabyte1027@users.noreply.github.com>
2021-03-04 15:22:36 -0800

    about added

--- nonretina-phone.js
+++ nonretina-phone.js
@@ -1,6 +1,16 @@
 function openset() {
 	document.getElementById('settings').style.left = '0px';
+	document.getElementById('about').style.display = 'block';
 }
 function closeset() {
 	document.getElementById('settings').style.left = '320px';
+	document.getElementById('about').style.display = 'none';
+}
+function openabout() {
+	document.getElementById('about').style.left = '0px';
+	document.getElementById('settings').style.left = '-320px';
+}
+function closeabout() {
+	document.getElementById('about').style.left = '320px';
+	document.getElementById('settings').style.left = '0px';
 }
